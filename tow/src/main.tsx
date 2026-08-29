42e9f7bc046115ebd4229326d7346a590d40653a
admytruk79-netizen <admytruk79@gmail.com>
2026-08-29 01:22:16 -0700

    fix(tow): load MapLibre stylesheet for case map

--- tow/src/main.tsx
+++ tow/src/main.tsx
@@ -1,1 +1,1 @@
-import {StrictMode} from 'react';import{createRoot}from'react-dom/client';import App from './App';import'./index.css';import'./local-map-frame.css';createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
+import {StrictMode} from 'react';import{createRoot}from'react-dom/client';import App from './App';import'maplibre-gl/dist/maplibre-gl.css';import'./index.css';import'./local-map-frame.css';createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
