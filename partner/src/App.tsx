1d289e3038ad8ee399b91ac431e68c8269a6b454
admytruk79-netizen <admytruk79@gmail.com>
2026-09-06 23:25:57 -0700

    Wire actionable DVI into Shop OS

--- partner/src/App.tsx
+++ partner/src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from './Dashboard';
 import { LocalMap } from './LocalMap';
 import { Login } from './Login';
 import { ShopOsWorkspace } from './ShopOsWorkspace';
+import { ShopOsDviControl } from './ShopOsDviControl';
 import { ShopOsFloorControl } from './ShopOsFloorControl';
 
 function Portal() {
@@ -21,7 +22,7 @@ function Portal() {
     return () => window.removeEventListener('popstate', onBack);
   }, [principal]);
 
-  return principal ? <><Dashboard /><div className="shell"><main className="mx-auto max-w-7xl px-4 pb-10 sm:px-6"><ShopOsWorkspace /><ShopOsFloorControl /></main></div><LocalMap /></> : <Login />;
+  return principal ? <><Dashboard /><div className="shell"><main className="mx-auto max-w-7xl px-4 pb-10 sm:px-6"><ShopOsWorkspace /><ShopOsDviControl /><ShopOsFloorControl /></main></div><LocalMap /></> : <Login />;
 }
 
 export default function App() {
